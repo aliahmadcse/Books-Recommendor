a4c5d3cd98595fe2d0991bb573437fc82590a1cc
aliahmadcse <aliahmadcse@gmail.com>
2019-12-04 20:08:16 +0500

    Adds search form and retrieve data

--- application/routes.py
+++ application/routes.py
@@ -1,7 +1,9 @@
 from application import app
-from flask import render_template
+from flask import render_template,request
 
-@app.route('/')
+@app.route('/',methods=['GET','POST'])
 def index():
+    searchItem=request.form.get('searchItem')
+    if searchItem:
+        return render_template('index.html',searchItem=searchItem)
     return render_template('index.html')
-    
