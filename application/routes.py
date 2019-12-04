2fe55f76ff0e6091e1d0036f4c33c8c248f67ede
aliahmadcse <aliahmadcse@gmail.com>
2019-12-04 23:01:30 +0500

    Adds pandas library

--- application/routes.py
+++ application/routes.py
@@ -1,9 +1,13 @@
 from application import app
-from flask import render_template,request
+from flask import render_template, request
+import pandas as pd
 
-@app.route('/',methods=['GET','POST'])
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    searchItem=request.form.get('searchItem')
-    if searchItem:
-        return render_template('index.html',searchItem=searchItem)
-    return render_template('index.html')
+    # searchItem=request.form.get('searchItem')
+    # if searchItem:
+    #     return render_template('index.html',searchItem=searchItem)
+    books = pd.read_csv('BX-Books.csv', sep=';',error_bad_lines=False, encoding="latin-1")
+    books.columns = ["ISBN", "Book-Title", "Book-Author", "Year-Of-Publication","Publisher", "Image-URL-S", "Image-URL-M", "Image-URL-L"]
+    return render_template('index.html',books=books)
