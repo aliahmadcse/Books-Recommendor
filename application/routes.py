e33d4011a7f18dcab1eef0a2fe90eb9410f98b87
aliahmadcse <aliahmadcse@gmail.com>
2019-12-05 00:11:37 +0500

    Adds related css

--- application/routes.py
+++ application/routes.py
@@ -5,9 +5,10 @@ import pandas as pd
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    # searchItem=request.form.get('searchItem')
-    # if searchItem:
-    #     return render_template('index.html',searchItem=searchItem)
     books = pd.read_csv('BX-Books.csv', sep=';',error_bad_lines=False, encoding="latin-1")
     books.columns = ["ISBN", "Book-Title", "Book-Author", "Year-Of-Publication","Publisher", "Image-URL-S", "Image-URL-M", "Image-URL-L"]
     return render_template('index.html',books=books.sample(n=20))
+    searchItem=request.form.get('searchItem')
+    if searchItem:
+        return render_template('index.html',searchItem=searchItem)
+    
